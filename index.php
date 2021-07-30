92bd78598326c791cadd3e76fb01ba9f1c092743
edi-15 <68310851+edi-15@users.noreply.github.com>
2021-07-29 22:31:24 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/4099277a03.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- <header class="content-header ancho">
+        <h1 class="titulo">Tareas de Matemática</h1>
+        <i id="btnmenu" class="fas fa-bars"></i>
+        <nav>
+            <a href="#" class="links-navs">Conceptos</a>
+            <a href="#" class="links-navs">Mapas mentales</a>
+        </nav>
+    </header>HEADER -->
+    <!--MENU-->
+        <div class="content-menu">
+                <h1 class="titulo">Tareas de matemáticas</h1>
+            <i id="btnmenu" class="fas fa-bars hidden"></i>
+        <nav class="menu">
+            <hr class="lineas">
+            <a href="#conceptos" class="links-nav">Conceptos</a>
+            <hr class="lineas">
+            <a href="#" class="links-nav">Mapas mentales</a>
+            <hr class="lineas">
+            <p href="#" class="links-nav nombre">Edison ALamos</p>
+            <hr class="lineas">
+        </nav>
+    </div>
+
+    <!--MENU-->
+
+    <div class="div-watermark" id="watermark">
+        <p class="watermark">Edison ÁLamos</p>
+    </div><!--MARCA DE AGUA-->
+
+    <section class="panel-principal">
+        <h1 class="titulo-panel">Conceptos 1°Semestre Matemática</h1>
+    </section>
+
+    <div class="container">
+
+    <div class="indice a">
+        <h3>Índice <span>EA</span></h3>
+        <div class="1unidad">
+            <span class="btn-open1" id="btn-open1">1° Unidad <i class="fas fa-sort-down"></i></span>
+            <hr class="lineas">
+        <ul class="lista-1unidad">
+            <li><a href="#nreales">Números reales</a></li>
+            <li><a href="#nracionales">Números racionales</a></li>
+            <li><a href="#nirracionales">Números irracionales</a></li>
+            <li><a href="#potencias">Potencias</a></li>
+            <li><a href="#renesimas">Raíces Enezimas</a></li>
+            <li><a href="#mrindice">Multiplicaciones de raíces de igual índice</a></li>
+            <li><a href="#drindice">División de raíces de igual índice</a></li>
+            <li><a href="#rraiz">Raíz de una raíz</a></li>
+            <li><a href="#rcion">Racionalización</a></li>
+            <li><a href="#log">Logaritmos</a></li>
+            <li class="mp"><a href="#mindmap">Mapa mental</a></li>
+        </ul>
+    </div><!--UNIDAD 1-->
+
+    <div class="2unidad">
+        <span class="btn-open2" id="btn-open2">2° Unidad <i class="fas fa-sort-down"></i></span>
+        <hr class="lineas">
+    <ul class="lista-2unidad">
+        <a><li href="#nreales">#</li></a>
+        <a><li href="#niracionales">#</li></a>
+        <a><li href="#"></li>#</a>
+        <a><li href="#"></li>#</a>
+        <a><li href="#"></li>#</a>
+        <a><li href="#"></li>#</a>
+        <a><li href="#"></li>#</a>
+    </ul>
+
+</div>
+    </div>
+
+    <main id="conceptos">
+        <h3>CONCEPTOS</h3>
+        <!--SECCION-->
+            <h4 id="nreales">Números reales</h4>
+        <div class="content-ejercicios">
+            <p>Es la unión de los números racionales e irracionales.</p>
+            <!-- <img class="img-ejercicios" src="https://p4.wallpaperbetter.com/wallpaper/330/785/205/equations-math-wallpaper-preview.jpg" alt=""> -->
+        </div>
+        <hr>
+        <!--SECCION-->
+        <!--SECCION-->
+        <h4 id="nracionales">Números racionales</h4>
+        <div class="content-ejercicios">
+            <p>Son todos los números que se pueden expresar en una fracción (decimales finitos, decimales periódicos y los decimales semiperiodicos).</p>
+            <!-- <img class="img-ejercicios" src="https://p4.wallpaperbetter.com/wallpaper/330/785/205/equations-math-wallpaper-preview.jpg" alt=""> -->
+        </div>
+        <!--SECCION-->
+        <!--SECCION-->
+            <h4 id="nirracionales">Números irracionales</h4>
+        <div class="content-ejercicios">
+            <p>Son todos los números con infinitos decimales.</p>
+            <!-- <img class="img-ejercicios" src="https://p4.wallpaperbetter.com/wallpaper/330/785/205/equations-math-wallpaper-preview.jpg" alt=""> -->
+        </div>
+        <!--SECCION-->
+        <hr>
+        <!--SECCION-->
+            <h4 id="potencias">Poténcias</h4>
+        <div class="content-ejercicios">
+            <p>Es multiplicar el mismo número por sí mismo varias veces. El número que multiplicamos se llama base y el exponente es el número de veces que multiplicamos.</p>
+            <!-- <img class="img-ejercicios" src="https://p4.wallpaperbetter.com/wallpaper/330/785/205/equations-math-wallpaper-preview.jpg" alt=""> -->
+        </div>
+        <!--SECCION-->
+        <hr>
+        <!--SECCION-->
+            <h4 id="renesimas">Raíces enésimas</h4>
+        <div class="content-ejercicios">
+            <p>Las raíces enésimas son lo que se multiplica n veces para tener el valor original</p>
+            <!-- <img class="img-ejercicios" src="https://p4.wallpaperbetter.com/wallpaper/330/785/205/equations-math-wallpaper-preview.jpg" alt=""> -->
+        </div>
+        <hr>
+        <!--SECCION-->
+        <!--SECCION-->
+            <h4 id="mrindice">Multiplicación de raíces con igual índice</h4>
+        <div class="content-ejercicios">
+            <p>Es la multiplicación de las cantidades subradicales conservándose el índice que tienen en común.</p>
+            <!-- <img class="img-ejercicios" src="https://p4.wallpaperbetter.com/wallpaper/330/785/205/equations-math-wallpaper-preview.jpg" alt=""> -->
+        </div>
+        <hr>
+        <!--SECCION-->
+        <!--SECCION-->
+            <h4 id="drindice">División de raíces con igual índice</h4>
+        <div class="content-ejercicios">
+            <p>Es la división de las cantidades subradicales conservándose el índice que tiene en común.</p>
+            <!-- <img class="img-ejercicios" src="https://p4.wallpaperbetter.com/wallpaper/330/785/205/equations-math-wallpaper-preview.jpg" alt=""> -->
+        </div>
+        <hr>
+        <!--SECCION-->
+        <!--SECCION-->
+            <h4 id="rraiz">Raíz de una raíz</h4>
+        <div class="content-ejercicios">
+            <p>Es la multiplicación de los índices de una raíz.</p>
+            <!-- <img class="img-ejercicios" src="https://p4.wallpaperbetter.com/wallpaper/330/785/205/equations-math-wallpaper-preview.jpg" alt=""> -->
+        </div>
+        <hr>
+        <!--SECCION-->
+        <!--SECCION-->
+            <h4 id="rcion">Racionalización</h4>
+        <div class="content-ejercicios">
+            <p>La racionalización consiste en quitar los radicales de un dominador, para hacerlas más fáciles.</p>
+            <!-- <img class="img-ejercicios" src="https://p4.wallpaperbetter.com/wallpaper/330/785/205/equations-math-wallpaper-preview.jpg" alt=""> -->
+        </div>
+        <hr>
+        <!--SECCION-->
+        <!--SECCION-->
+            <h4 id="log">Logaritmos</h4>
+        <div class="content-ejercicios">
+            <p>El logaritmo de un número, en una base dada, es el exponente al cual se debe elevar la base para obtener el número.</p>
+            <!-- <img class="img-ejercicios" src="https://p4.wallpaperbetter.com/wallpaper/330/785/205/equations-math-wallpaper-preview.jpg" alt=""> -->
+        </div>
+        <hr>
+        <!--SECCION-->
+        <div id="mindmap" class="cmindmap">
+            <h3>Mapa mental</h3>
+            <div class="mindmap">
+                <p class="opnminmap" id="opnminmap">Abrir imagen</p>
+                <!-- <img class="imgmindmap" src="https://p4.wallpaperbetter.com/wallpaper/330/785/205/equations-math-wallpaper-preview.jpg" alt=""> -->
+            </div>
+            <div class="imgmindmap">
+                <div class="content-img">
+                    <i class="fas fa-times" id="closeimg"></i>
+            <img class="img" src="img/mindmap1.png" alt="">
+                </div>
+            </div>
+
+        </div>
+    </main>
+</div>
+
+<footer>
+    <div class="f">
+        <p>Edison ÁLamos 2021</p>
+    </div>
+</footer>
+
+<script src="main.js"></script>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,88 @@
+let ubicacionprincipal = window.pageYOffset;
+window.onscroll = function() {
+    let desplazamiento_actual = window.pageYOffset;
+    if(ubicacionprincipal >= desplazamiento_actual){
+        document.getElementById('watermark').style.top = '70px';
+    }
+    else {
+        document.getElementById('watermark').style.top = '-300px';
+        document.getElementById('watermark').style.transition = 'all 0.5s';
+    }
+    ubicacionprincipal = desplazamiento_actual;
+}
+
+
+
+const btn = document.getElementById('btnmenu');
+const menu = document.querySelector('.menu');
+
+btn.addEventListener('click', ()=> {
+    menu.classList.toggle('mostrar')
+});
+
+//UNIDAD 1
+const btnunidad1 = document.getElementById('btn-open1');
+const lista1 = document.querySelector('.lista-1unidad');
+
+btnunidad1.addEventListener('click', ()=> {
+    lista1.classList.toggle('open')
+});
+
+//UNIDAD 2
+const btnunidad2 = document.getElementById('btn-open2');
+const lista2 = document.querySelector('.lista-2unidad');
+
+btnunidad2.addEventListener('click', ()=> {
+    lista2.classList.toggle('open')
+});
+
+
+
+// //IMAGEN OPEN
+
+
+
+// const btnimg = document.getElementById('opnminmap');
+// const img = document.querySelector('.imgmindmap');
+
+// btnimg.addEventListener('click', ()=> {
+//     img.classList.toggle('abririmg')
+// });
+
+// const closeimg = document.getElementById('closeimg');
+// const img = document.querySelector('.imgmindmap');
+
+// closeimg.addEventListener('click', ()=> {
+//     img.classList.toggle('cerrarimg')
+// });
+
+// if(img = classList.toggle('abririmg')  ) {
+//     closeimg.addEventListener('click', ()=> {
+//         img.classList.toggle('cerrarimg')
+//     });
+// }
+
+
+
+// const imgs = document.querySelector('.imgmindmap');
+
+// closeimg.addEventListener('click', ()=> {
+//     imgs.classList.toggle('cerrarimg')
+// });
+
+
+
+
+
+//MENU
+var btnimg = document.getElementById('opnminmap');
+var img = document.querySelector('.imgmindmap');
+var closeimg = document.getElementById('closeimg');
+
+btnimg.addEventListener('click', function(){
+    img.classList.toggle('abririmg');
+})
+
+closeimg.addEventListener('click', function(){
+    img.classList.toggle('abririmg');
+})
